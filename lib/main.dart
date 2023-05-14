fe5dd09d299f3b2e2adbcd62d33bacbf35a80daa
Nikita Max <iamlegkiy@gmail.com>
2023-05-14 18:40:01 +0800

    added random coasts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,15 +15,15 @@ class MainPage extends StatelessWidget {
       title: 'Exchange Rates',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
-          backgroundColor: Color.fromARGB(255, 34, 33, 48),
-          titleTextStyle: TextStyle(color: Colors.white, fontSize: 22),
-        ),
+            backgroundColor: Color.fromARGB(255, 34, 33, 48),
+            titleTextStyle: TextStyle(color: Colors.white, fontSize: 22),
+            iconTheme: IconThemeData(color: Colors.white)),
         scaffoldBackgroundColor: const Color.fromARGB(255, 34, 41, 61),
         primarySwatch: Colors.yellow,
         dividerTheme: const DividerThemeData(color: Colors.white12, space: 1),
         listTileTheme: const ListTileThemeData(
-          iconColor: Colors.white,
-        ),
+            iconColor: Colors.white,
+            contentPadding: EdgeInsets.only(left: 20, right: 20)),
         textTheme: const TextTheme(
           bodyMedium: TextStyle(
             color: Colors.white,
@@ -34,119 +36,102 @@ class MainPage extends StatelessWidget {
         ),
         // useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Exchange Rates'),
+      routes: {
+        '/': (context) => const ExcahgeListScrean(),
+        '/coin:': (context) => const CoinScrean()
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class ExcahgeListScrean extends StatefulWidget {
+  const ExcahgeListScrean({
+    super.key,
+  });
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ExcahgeListScrean> createState() => _ExcahgeListScreanState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ExcahgeListScreanState extends State<ExcahgeListScrean> {
   int activeIndex = -1;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     final textBody = Theme.of(context).textTheme.bodyMedium;
     final subtitlText = Theme.of(context).textTheme.bodySmall;
+    final randomCoast = Random().nextInt(10000) + 20;
 
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(widget.title),
+        title: const Text('Exchange Rates'),
       ),
       body: ListView.separated(
-        itemCount: 20,
-        separatorBuilder: (context, index) => Divider(),
-        itemBuilder: (context, index) => ListTile(
-          leading: Image.network(
-            'https://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png',
-            height: 40,
-          ),
+          itemCount: 20,
+          separatorBuilder: (context, index) => const Divider(),
+          itemBuilder: (context, index) {
+            String coinName = index.toString();
+
+            return ListTile(
+              leading: Image.network(
+                'https://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png',
+                height: 40,
+              ),
+              title: Text(
+                'value $index',
+                style: textBody,
+              ),
+              subtitle: Text(
+                'coast: $randomCoast \$',
+                style: subtitlText,
+              ),
+              trailing: const Icon(
+                Icons.data_thresholding_outlined,
+                size: 50,
+              ),
+              onTap: () {
+                Navigator.of(context).pushNamed('/coin:', arguments: coinName);
+              },
+            );
+          }),
+    );
+  }
+}
+
+class CoinScrean extends StatefulWidget {
+  const CoinScrean({
+    super.key,
+  });
+
+  @override
+  State<CoinScrean> createState() => _CoinScreanState();
+}
+
+class _CoinScreanState extends State<CoinScrean> {
+  String? coinName;
+
+  @override
+  void didChangeDependencies() {
+    final args = ModalRoute.of(context)?.settings.arguments;
+    assert(args != null && args is String, 'ERROR: Invalid arguments');
+
+    coinName = args as String;
+    setState(() {});
+    super.didChangeDependencies();
+    // if (args != null || args is! String) {
+    // } else {
+    //   return;
+    // }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
           title: Text(
-            'value $index',
-            style: textBody,
-          ),
-          subtitle: Text(
-            'coast: $_counter \$',
-            style: subtitlText,
-          ),
-          trailing: const Icon(
-            Icons.data_thresholding_outlined,
-            size: 50,
-          ),
-        ),
-      ),
-      // body: ListView.builder(
-      //   itemCount: 10,
-      //   itemBuilder: (context, index) {
-      //     return Column(children: [
-      //       GestureDetector(
-      //         onTap: () => setState(() {
-      //           if (activeIndex == index) {
-      //             activeIndex = -1;
-      //             return;
-      //           }
-      //           activeIndex = index;
-      //         }),
-      //         child: Container(
-      //           height: 40,
-      //           width: double.infinity,
-      //           decoration: BoxDecoration(
-      //             color: Colors.blueGrey,
-      //             borderRadius: BorderRadius.only(
-      //               topLeft: Radius.circular(index == 0 ? 10 : 0),
-      //               topRight: Radius.circular(index == 0 ? 10 : 0),
-      //               bottomLeft: Radius.circular(index == 9
-      //                   ? activeIndex == 9
-      //                       ? 0
-      //                       : 10
-      //                   : 0),
-      //               bottomRight: Radius.circular(index == 9
-      //                   ? activeIndex == 9
-      //                       ? 0
-      //                       : 10
-      //                   : 0),
-      //             ),
-      //           ),
-      //           margin: const EdgeInsets.only(left: 10, right: 10),
-      //           padding: const EdgeInsets.all(5),
-      //           child: Text('data $index'),
-      //         ),
-      //       ),
-      //       AnimatedContainer(
-      //         duration: const Duration(
-      //           milliseconds: 200,
-      //         ),
-      //         curve: Curves.easeInOut,
-      //         width: double.infinity,
-      //         height: activeIndex == index ? 200 : 0,
-      //         margin: const EdgeInsets.only(left: 10, right: 10),
-      //         decoration: const BoxDecoration(
-      //             border: Border(
-      //                 bottom: BorderSide(width: 1, color: Colors.black))),
-      //         child: Text('body $_counter'),
-      //       ),
-      //     ]);
-      //   },
-      // ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        'Coins ${coinName ?? '...'}',
+      )),
     );
   }
 }
